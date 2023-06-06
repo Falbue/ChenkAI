be2c037a3dcda906ab1a55a7ae59eda0d1754a73
Falbue <cyansair05@gmail.com>
2023-06-06 13:42:04 +0500

    Изменил версию в описании

--- Chat_good.py
+++ Chat_good.py
@@ -45,7 +45,7 @@ welcome_text = """Привет. Это ChenkGPT
 4. По всем вопросам обращаться на почту: ChenkGPT@gmail.com
 
 Falbue <3
-ver: 0.7"""
+ver: 0.8.1"""
 
 # настройка кнопок
 def create_button(frame, text, command):
