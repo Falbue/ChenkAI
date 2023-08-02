34b0d8e4508658ce3e71c8b3f78886f4bdbacafe
Falbue <64151271+Falbue@users.noreply.github.com>
2023-08-02 20:13:23 +0500

    Улучшен установщик

--- installer.py
+++ installer.py
@@ -6,7 +6,7 @@ import tkinter as tk
 import threading
 import time
 
-token_git = 'klt_CxpoPFvnOs4zvMpUkWakMVlCBPC5KK0Fj3ET'
+token_git = 'klt_bpGFDtb4bwPdlm2FjM85HZXyGMvCAV0SIl6u'
 shift = 4
 
 def decrypt(text_api, shift):
@@ -27,26 +27,23 @@ token_git = text_api
 print(token_git)
 
 def update():
-    # путь к рабочему столу
     desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-    # имя новой папки
     folder_name = 'ChenkGPT'
-    # полный путь к новой папке
     new_folder_path = os.path.join(desktop_path, folder_name)
 
-    # проверяем, существует ли уже папка
     if not os.path.exists(new_folder_path):
-        # создание новой папки
         os.mkdir(new_folder_path)
     else:
         print(f"Папка {folder_name} уже существует на рабочем столе.")
-
 
     g = Github(token_git)
     repo_name = "Falbue/ChenkGPT"
     repo = g.get_repo(repo_name)
     releases = repo.get_releases()
     latest_release = releases[0]
+    assets = list(latest_release.get_assets())
+    total_files = len(assets)
+    downloaded_files = 0
 
     for asset in latest_release.get_assets():
         if asset.name.endswith(".exe"):
@@ -54,9 +51,15 @@ def update():
             r = requests.get(file_url)
             with open(f"C:/Users/{os.getlogin()}/Desktop/ChenkGPT/{asset.name}", "wb") as f:
                 f.write(r.content)
-                lbl.pack_forget()
-                btn.pack(expand=True)
-    
+                downloaded_files += 1
+                progress = f"Загрузка файлов: {downloaded_files}/{total_files}"
+                lbl_progress.config(text=progress)
+                btn_cancel.config(state=tk.DISABLED)
+
+                if downloaded_files == total_files:
+                    lbl_progress.config(text="Обновление завершено.")
+                    btn_run.pack(expand=True)
+                    btn_cancel.pack_forget()
 
 def close():
     path = f"C:/Users/{os.getlogin()}/Desktop/ChenkGPT/ChenkGPT.exe"
@@ -64,21 +67,39 @@ def close():
     time.sleep(1)
     root.destroy()
 
-thread = threading.Thread(target=update)
-thread.start()
+def cancel():
+    lbl_progress.config(text="Обновление отменено.")
+    btn_cancel.config(state=tk.DISABLED)
+    btn_run.pack(expand=True)
+    btn_cancel.pack_forget()
 
 root = tk.Tk()
-root.geometry('400x200')
-lbl = Label(
-    text = 'Обновляется...',
-    font = ("Arial", 16, 'bold'))
-lbl.pack(expand=True)
-btn = Button(
+root.geometry('300x200')
+
+lbl_progress = Label(
+    text='Обновление...',
+    font=("Arial", 16, 'bold'))
+lbl_progress.pack(expand=True)
+
+btn_cancel = Button(
     font=("Arial", 16),
-    text='Закрыть',
+    text='Отменить',
+    command=cancel,
+    relief='solid',
+    border=1,
+    highlightbackground="black")
+btn_cancel.pack(expand=True)
+
+btn_run = Button(
+    font=("Arial", 16),
+    text='Запустить',
     command=close,
     relief='solid',
     border=1,
     highlightbackground="black")
 
+thread = threading.Thread(target=update)
+thread.start()
+
+root.protocol("WM_DELETE_WINDOW", close)
 root.mainloop()
