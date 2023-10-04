2aa37980af6a1af10b3a2eda2793bb78185be8e6
Falbue <cyansair05@gmail.com>
2023-10-04 19:28:33 +0500

    Дизайн элементов вынесен в отдельные файлы

--- main.py
+++ main.py
@@ -91,48 +91,6 @@ def close_info():
     shift_frame.pack(side="top", anchor="nw", fill='x')
     sign()
 
-
-# настройка кнопок
-def create_button(frame, text, command):
-    button = Button(
-        frame,
-        activebackground=bg_color_dark,
-        font=(fonts, font_size),
-        bg='white',
-        fg=fg_color,
-        text=text,
-        command=command,
-        relief='solid',
-        border=1,
-        highlightbackground=fg_color
-    )
-    button.bind("<Enter>", lambda event: button.configure(bg=bg_color_dark))
-    button.bind("<Leave>", lambda event: button.configure(bg=bg_color))
-    return button
-
-def entry_design(frame):
-    entry = Entry(
-        frame,
-        font=(fonts, 16),
-        bg='white',
-        fg=fg_color,
-        relief='solid',
-        border=1,
-    )
-    entry.bind("<FocusIn>", lambda event: entry.configure(bg=bg_color_dark))
-    entry.bind("<FocusOut>", lambda event: entry.configure(bg=bg_color))
-    return entry
-
-def label_design(frame, text):
-    label = Label(
-        frame,
-        text=text,
-        font=('Arial',14)
-        )
-    return label
-
-
-
 # Определяем функцию для шифрования текста
 def encrypt(text_api, shift):
     result = ""
